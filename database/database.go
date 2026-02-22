24aa24553c32684f719fbc4afddbf218ca107cca
Möhsün Babayev <mohsun.babayev@protonmail.com>
2026-02-22 17:13:02 +0400

    chore: clean up database migrations

--- database/database.go
+++ database/database.go
@@ -2,14 +2,22 @@ package database
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func PrepareDatabase(ctx context.Context, username string, password string, host string, port int, database string) (*pgxpool.Pool, error) {
-	RunMigrations()
+	err := runMigrations(username, password, host, port, database)
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Printf("cannot perform database migration: %v\n", err)
+	}
+
 	pool, err := pgxpool.New(ctx, fmt.Sprintf("postgres://%s:%s@%s:%d/%s", username, password, host, port, database))
 	if err != nil {
 		return nil, fmt.Errorf("failed to establish db connection: %w", err)
@@ -17,4 +25,12 @@ func PrepareDatabase(ctx context.Context, username string, password string, host
 		log.Println("successfully acquired db connection")
 	}
 	return pool, nil
+}
+
+func runMigrations(username string, password string, host string, port int, database string) error {
+	m, err := migrate.New("file://database/migrations", fmt.Sprintf("pgx5://%s:%s@%s:%d/%s", username, password, host, port, database))
+	if err != nil {
+		return err
+	}
+	return m.Up()
 }
